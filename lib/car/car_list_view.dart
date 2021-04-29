025ec2e2148ca3079b9d7e9cb45b2e2b1ff29262
Otoniel Duarte <otonielduarte2@gmail.com>
2021-04-29 13:02:53 -0300

    share

--- lib/car/car_list_view.dart
+++ lib/car/car_list_view.dart
@@ -1,8 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+
 import 'package:cars/car/car.dart';
 import 'package:cars/car/details_car_page.dart';
 import 'package:cars/shared/util/nav.dart';
-import 'package:flutter/material.dart';
 
 class CarsListView extends StatelessWidget {
   final List<Car> cars;
@@ -11,53 +13,52 @@ class CarsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: _futureBuilder(),
-    );
-  }
-
-  _futureBuilder() {
     return ListView.builder(
       itemCount: cars.length,
-      itemBuilder: (context, index) {
+      itemBuilder: (listViewContext, index) {
         final Car car = cars[index];
-        return Padding(
+        return Container(
           padding: const EdgeInsets.only(right: 8.0, left: 8.0),
+          height: 280,
           child: Card(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: _image(car.urlFoto ?? ''),
-                ),
-                ListTile(
-                  title: Text(
-                    car.nome ?? '',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 16),
+            child: InkWell(
+              onTap: () => _onClickCar(listViewContext, car),
+              onLongPress: () => _onLongClickCar(listViewContext, car),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: _image(car.urlFoto ?? ''),
                   ),
-                  subtitle: Text(
-                    car.desc ?? '',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
+                  ListTile(
+                    title: Text(
+                      car.nome ?? '',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    subtitle: Text(
+                      car.desc ?? '',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () => push(context, DetailCarPage(car)),
-                      child: Text('Detalhes'),
-                    ),
-                    TextButton(
-                      onPressed: () => print('share'),
-                      child: Text('Share'),
-                    ),
-                  ],
-                )
-                // Text(car.nome),
-              ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () => _onClickCar(context, car),
+                        child: Text('Detalhes'),
+                      ),
+                      TextButton(
+                        onPressed: () => _onClickShare(context, car),
+                        child: Text('Share'),
+                      ),
+                    ],
+                  )
+                  // Text(car.nome),
+                ],
+              ),
             ),
           ),
         );
@@ -71,5 +72,55 @@ class CarsListView extends StatelessWidget {
         height: 120,
         imageUrl: url,
         errorWidget: (context, url, error) => Icon(Icons.error));
+  }
+
+  _onClickCar(BuildContext context, Car car) {
+    push(context, DetailCarPage(car));
+  }
+
+  _onLongClickCar(BuildContext context, Car car) {
+    showModalBottomSheet(
+      context: context,
+      builder: (dialogContext) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                '${car.nome}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.directions_car),
+              title: Text("Detalhes"),
+              onTap: () {
+                Navigator.pop(dialogContext);
+                _onClickCar(context, car);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.share),
+              onTap: () {
+                _onClickShare(context, car);
+                Navigator.pop(dialogContext);
+              },
+              title: Text("Share"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  _onClickShare(BuildContext context, Car car) async {
+    final box = context.findRenderObject() as RenderBox?;
+    await Share.share(
+        "Project Cars - Flutter ${car.nome} modelo ${car.tipo} acertos",
+        sharePositionOrigin: box!.localToGlobal(Offset.zero) & box.size);
   }
 }
